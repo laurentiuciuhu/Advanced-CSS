a6055ad3b4f1750dc35f3b28e9981061f41e7f97
laurentiuciuhu <33173001+laurentiuciuhu@users.noreply.github.com>
2021-03-20 02:17:30 -0700

    Add files via upload

---
+++ Curs 5/light-and-dark/site.js
@@ -0,0 +1,31 @@
+document.querySelector("#switch")
+    .addEventListener("click", changeTheme)
+
+var theme = localStorage.getItem("theme")
+setTheme(theme)
+
+function setTheme() {
+    let css = document.querySelector("#darkcss")
+    css.disabled = theme == "light" ? true : false
+
+    // let symbol = css.disabled ? "&#9789;" : "&#9788;"
+    // document.querySelector("#switch").innerHTML = symbol
+
+    localStorage.setItem("theme", theme)
+}
+
+function changeTheme() {
+    theme = theme == "light" ? "dark": "light"
+    setTheme(theme)
+
+    // css.disabled = css.disabled ? false : true
+
+    // switch(css.dusabled) {
+    //    case true:
+    //        css.disabled = false
+    //       break
+
+    //    default:
+    //        css.disabled = true
+    //}
+}
